db1ff2632cde243a2518bd32774bfefbe8de506f
Hegde <hegdepr@wft.root.loc>
2015-12-28 17:16:40 +0530

    Rev3

---
+++ LicenseCheck/Helper/Helper.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Configuration;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Lowis_Reports_Testing.ObjectLibrary
+{
+    class Helper
+    {
+        public enum CompareType
+        {
+            Equal,
+            Contians,
+            Tolerance,
+            DecimalRound
+        }
+        private DataTable _dtRep = new DataTable();
+        public DataTable dtRep
+        {
+            get { return _dtRep; }
+            set { _dtRep = value; }
+        }
+        int counter = 1;
+        public DataTable dtFromExcelFile(string filepath, string sheetname)
+        {
+            try
+            {
+                DataTable dtble = new DataTable();
+
+                OdbcConnection oconn = new OdbcConnection();
+                oconn.ConnectionString = ConfigurationManager.ConnectionStrings["ReportLinks"].ToString() + filepath;
+                string odbccmdtext = "Select * from [" + sheetname + "$]";
+                OdbcCommand ocmd = new OdbcCommand(odbccmdtext, oconn);
+                oconn.Open();
+                OdbcDataAdapter da = new OdbcDataAdapter(ocmd);
+                da.Fill(dtble);
+                oconn.Close();
+                return dtble;
+            }
+            catch
+            {
+                throw new Exception();
+            }
+
+        }
+
+        public DataTable dtFromExcelFile(string filepath, string sheetname, string filtercolumnName, string filtervalue)
+        {
+            try
+            {
+                DataTable dtble = new DataTable();
+                //string strFileName = filepath;
+                //string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;";
+                //strConn += "Data Source= " + strFileName + "; Extended Properties='Excel 8.0;HDR=No;IMEX=1'";
+                this.LogtoTextFile("Inside Get Excel Connection");
+                OdbcConnection oconn = new OdbcConnection();
+                oconn.ConnectionString = ConfigurationManager.ConnectionStrings["ReportLinks"].ToString() + filepath;
+                string odbccmdtext = "Select * from [" + sheetname + "$]  where " + filtercolumnName + "='" + filtervalue + "'";
+                OdbcCommand ocmd = new OdbcCommand(odbccmdtext, oconn);
+                try
+                {
+                    oconn.Open();
+                }
+                catch (Exception ex)
+                {
+                    this.LogtoTextFile("c error" + ex.Message);
+                }
+                OdbcDataAdapter da = new OdbcDataAdapter(ocmd);
+                da.Fill(dtble);
+                oconn.Close();
+
+                //OleDbConnection oconn = new OleDbConnection();
+                //oconn.ConnectionString = strConn;
+                //string odbccmdtext = "Select * from [" + sheetname + "$]  where " + filtercolumnName + "='" + filtervalue + "'";
+                //OleDbCommand ocmd = new OleDbCommand(odbccmdtext, oconn);
+                //oconn.Open();
+                //OleDbDataAdapter da = new OleDbDataAdapter(ocmd);
+                //da.Fill(dtble);
+                //oconn.Close();
+
+                this.LogtoTextFile("Out from  Get Excel Connection");
+                return dtble;
+            }
+            catch
+            {
+                throw new Exception();
+            }
+
+        }
+        public void AreEqual(string tcnameid, string linkName, string VerifyParameter, string exp, string act, CompareType compareOperator)
+        {
+
+            if (counter == 1)
+            {
+                dtRep.Columns.Add("TestCaseNameORId");
+                dtRep.Columns.Add("LinkName");
+                dtRep.Columns.Add("VerifyParameter");
+                dtRep.Columns.Add("Expected");
+                dtRep.Columns.Add("Actual");
+                dtRep.Columns.Add("Result");
+            }
+            DataRow dr = dtRep.NewRow();
+            switch (compareOperator.ToString().ToLower())
+            #region OperatorsofVerify
+            {
+
+                case "equal":
+                    {
+                        if (exp.Length > 0)
+                        {
+                            if (exp.ToLower().Trim() == act.ToLower().Trim())
+                            {
+                                dr["TestCaseNameORId"] = tcnameid;
+                                dr["LinkName"] = linkName;
+                                dr["VerifyParameter"] = VerifyParameter;
+                                dr["Expected"] = exp;
+                                dr["Actual"] = trimcustom(act);
+                                dr["Result"] = "Pass";
+                            }
+                            else
+                            {
+                                dr["TestCaseNameORId"] = tcnameid;
+                                dr["LinkName"] = linkName;
+                                dr["VerifyParameter"] = VerifyParameter;
+                                dr["Expected"] = exp;
+                                dr["Actual"] = trimcustom(act);
+                                dr["Result"] = "Fail";
+                            }
+                        }
+                        break;
+                    }
+                case "contains":
+                    {
+                        if (exp.Length > 0)
+                        {
+                            if (cleanIntermediateWhiteSpaces(act).ToLower().Contains(cleanIntermediateWhiteSpaces(exp).ToLower()))
+                            {
+                                dr["TestCaseNameORId"] = tcnameid;
+                                dr["LinkName"] = linkName;
+                                dr["VerifyParameter"] = VerifyParameter;
+                                dr["Expected"] = exp;
+                                dr["Actual"] = trimcustom(act);
+                                dr["Result"] = "Pass";
+                            }
+                            else
+                            {
+                                dr["TestCaseNameORId"] = tcnameid;
+                                dr["LinkName"] = linkName;
+                                dr["VerifyParameter"] = VerifyParameter;
+                                dr["Expected"] = exp;
+                                dr["Actual"] = trimcustom(act);
+                                dr["Result"] = "Fail";
+                            }
+                        }
+                        break;
+                    }
+
+                case "tolerance":
+                    {
+                        break;
+                    }
+
+                case "decimalround":
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+
+            }
+            #endregion
+
+
+            if (dr["TestCaseNameORId"].ToString().Length > 0)
+            {
+                dtRep.Rows.Add(dr);
+            }
+            counter++;
+        }
+
+        public void LogtoFileCSV(DataTable dtin)
+        {
+            char delm = '\u0022';
+            StringBuilder sb = new StringBuilder();
+            if (dtin.Rows.Count > 0)
+            {
+                if (System.IO.File.Exists(ConfigurationManager.AppSettings["logfile"]) == false)
+                {
+                    //Adding Header Row only once
+                    for (int kk = 0; kk < dtin.Columns.Count; kk++)
+                    {
+                        sb.Append(delm + dtin.Columns[kk].ColumnName.ToString() + delm + ",");
+
+                    }
+
+                    sb.Append(Environment.NewLine);
+                }
+
+                for (int i = 0; i < dtin.Rows.Count; i++)
+                {
+                    for (int kk = 0; kk < dtin.Columns.Count; kk++)
+                    {
+                        sb.Append(delm + dtin.Rows[i][kk].ToString() + delm + ",");
+                    }
+                    sb.Append(Environment.NewLine);
+                }
+
+                System.IO.File.AppendAllText(ConfigurationManager.AppSettings["logfile"], sb.ToString());
+            }
+        }
+
+        public void LogTabletoWordFile(DataTable dtin)
+        {
+            try
+            {
+                this.LogtoTextFile("Inside LogTabletoWordFile method");
+                object oMissing = System.Reflection.Missing.Value;
+                object oEndOfDoc = "\\endofdoc";
+                Microsoft.Office.Interop.Word._Application objWord;
+                Microsoft.Office.Interop.Word._Document objDoc;
+                objWord = new Microsoft.Office.Interop.Word.Application();
+                objWord.Visible = false;
+                objDoc = objWord.Documents.Add(ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing);
+                int i = 0;
+                int j = 0;
+                Microsoft.Office.Interop.Word.Table objTable;
+                Microsoft.Office.Interop.Word.Range wrdRng = objDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+                objTable = objDoc.Tables.Add(wrdRng, dtin.Rows.Count, dtin.Columns.Count, ref oMissing, ref oMissing);
+                objTable.Range.ParagraphFormat.SpaceAfter = 4;
+                for (j = 1; j <= dtin.Columns.Count; j++)
+                {
+                    objTable.Cell(1, j).Range.Text = dtin.Columns[j - 1].ColumnName.ToString();
+                    objTable.Cell(1, j).Borders[Word.WdBorderType.wdBorderLeft].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                    objTable.Cell(1, j).Borders[Word.WdBorderType.wdBorderRight].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                    objTable.Cell(1, j).Borders[Word.WdBorderType.wdBorderTop].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                    objTable.Cell(1, j).Borders[Word.WdBorderType.wdBorderBottom].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                }
+                objTable.Rows.Add();
+                for (i = 2; i <= dtin.Rows.Count+1;i++)
+                {
+                    for (j = 1; j <= dtin.Columns.Count; j++)
+                    {
+                        objTable.Cell(i, j).Range.Text = dtin.Rows[i - 2][j - 1].ToString();
+                        objTable.Cell(i, j).Range.Borders[Word.WdBorderType.wdBorderLeft].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                        objTable.Cell(i, j).Range.Borders[Word.WdBorderType.wdBorderRight].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                        objTable.Cell(i, j).Range.Borders[Word.WdBorderType.wdBorderTop].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                        objTable.Cell(i, j).Range.Borders[Word.WdBorderType.wdBorderBottom].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                    }
+                    objTable.Rows.Add();
+                }
+                objTable.Rows[1].Range.Font.ColorIndex = Word.WdColorIndex.wdBlue;
+                objTable.Rows[1].Range.Font.Bold = 1;
+                objTable.Rows[1].Range.Font.Italic = 1;
+                this.LogtoTextFile("Before Save working fine");
+                //objTable.Borders[Word.WdBorderType.wdBorderLeft].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                //objTable.Borders[Word.WdBorderType.wdBorderRight].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                //objTable.Borders[Word.WdBorderType.wdBorderTop].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                //objTable.Borders[Word.WdBorderType.wdBorderBottom].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                objDoc.SaveAs(ConfigurationManager.AppSettings["wordfile"]);
+                objDoc.Close();
+            }
+            catch(Exception ex)
+            {
+                this.LogtoTextFile("Got Exception from method [LogTabletoWordFile] " + ex.Message);
+            }
+
+        }
+
+        public void UpdateExcelFileColumn(string filepath, string sheetName, string columnName, string columnValue, string filterColName, string filterColValue)
+        {
+            OdbcConnection oconn = null;
+            try
+            {
+                oconn = new OdbcConnection();
+                oconn.ConnectionString = ConfigurationManager.ConnectionStrings["ReportLinks"].ToString() + filepath;
+                string odbccmdtext = "Update [" + sheetName + "$] Set " + columnName + "='" + columnValue + "' where " + filterColName + "='" + filterColValue + "' ";
+                OdbcCommand ocmd = new OdbcCommand(odbccmdtext, oconn);
+                oconn.Open();
+                ocmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                this.LogtoTextFile("Exception from update " + ex.Message);
+
+            }
+            finally
+            {
+                oconn.Close();
+            }
+
+        }
+
+        private string trimcustom(string inp)
+        {
+            string op = "";
+            char[] chartotrim = { ' ', '\n', '\t' };
+            op = inp.Trim(chartotrim);
+            string fop = op.Replace('\n', ' ');
+            fop = fop.Replace('\r', ' ');
+            if (fop.Length > 255)
+            {
+                // trim charts to 255 only 
+                fop = fop.Substring(0, 255);
+            }
+            return fop;
+        }
+        public void LogtoTextFile(string msg)
+        {
+            System.IO.File.AppendAllText(ConfigurationManager.AppSettings["logtextfile"], "[" + System.DateTime.Now.ToString() + "] :" + msg + System.Environment.NewLine);
+
+
+        }
+        private string cleanIntermediateWhiteSpaces(string strinput)
+        {
+
+            string pattn = "\\s+";
+            Regex re = new Regex(pattn);
+            string retstring = re.Replace(strinput, " ");
+
+            string op = "";
+            char[] chartotrim = { ' ', '\n', '\t' };
+            op = retstring.Trim(chartotrim);
+            string fop = op.Replace('\n', ' ');
+            fop = fop.Replace('\r', ' ');
+            return retstring;
+
+        }
+
+        
+    }
+}
---
+++ LicenseCheck/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Management;
+using System.Management.Instrumentation;
+using System.Diagnostics;
+
+
+
+
+
+namespace LicenseCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = ConfigurationManager.AppSettings["excelfilepath"];
+            string listTo = ConfigurationManager.AppSettings["listto"];
+            string listCC = ConfigurationManager.AppSettings["listcc"];
+            string strmsgfile = ConfigurationManager.AppSettings["subjecttextfile1"];
+            string strmsgfileno = ConfigurationManager.AppSettings["subjecttextfile2"];
+            Console.WriteLine("Getting Data from excel");
+            Lowis_Reports_Testing.ObjectLibrary.Helper hlpr = new Lowis_Reports_Testing.ObjectLibrary.Helper();
+            DataTable dtlictable = hlpr.dtFromExcelFile(filepath, "Sheet1");
+            DateTime datesevendaysfromnow = DateTime.Now.AddDays(7.00);
+            DataTable dtoutput = new DataTable();
+            dtoutput.Columns.Add("MachineOwner");
+            dtoutput.Columns.Add("MachineName");
+            dtoutput.Columns.Add("ProductName");
+            dtoutput.Columns.Add("Status");
+            dtoutput.Columns.Add("ExpiryDate");
+            StringBuilder sbout = new StringBuilder();
+            string[] arrProducts = new string[] { "Matbal", "WellFlo", "PanSystem", "RFC", "Reo", "PVTFlex", "Lowis" };
+            foreach (DataRow dr in dtlictable.Rows)
+            {
+                bool flagemty = false;
+                Console.WriteLine("Processing for Machine  {0}", dr["MACHINE NAME"].ToString());
+                if (dr["Active"].ToString().ToLower() == "y" )
+                {
+                    #region checkdateofeachproduct
+                    
+                    foreach (string product in arrProducts)
+                    {
+                        DataRow dro = dtoutput.NewRow();
+                        string expDateProduct = dr[product].ToString();
+                        try
+                        {
+                            DateTime dtExp = DateTime.Parse(expDateProduct);
+
+                            if (dtExp < datesevendaysfromnow)
+                            {
+                                dro["MachineOwner"] = dr["MACHINE OWNER"].ToString();
+                                dro["MachineName"] = dr["MACHINE NAME"].ToString();
+                                dro["ProductName"] = product;
+                                dro["Status"] = "Will Expire";
+                                dro["ExpiryDate"] = dtExp.ToString("dd-MMM-yyyy");
+                                flagemty = true;
+
+                            }
+
+                        }
+                        catch(Exception ex)
+                        {
+                            flagemty = false;
+                            Console.WriteLine("Expetion " + ex.Message);
+                        }
+                        if (flagemty)
+                        {
+                            dtoutput.Rows.Add(dro);
+                        }
+
+                    }
+                    #endregion
+                    
+                }
+            }
+            hlpr.LogTabletoWordFile(dtoutput);
+
+            TerminateProcessByForce("WINWORD*");
+           
+            if (dtoutput.Rows.Count > 0)
+            {
+                SendEmailNotification(ConfigurationManager.AppSettings["wordfile"],listTo,listCC, ReadTextfromTextFile(strmsgfile));
+            }
+            else
+            {
+                SendEmailNotification(null, listTo, listCC, ReadTextfromTextFile(strmsgfileno));
+            }
+            TerminateProcessByForce("WINWORD*");
+            TaskKill("WINWORD*");
+        }
+
+        public static void SendEmailNotification(string attachmentfilename, string ListTo ,string ListCC, string messagebody)
+        {
+            if (ConfigurationManager.AppSettings["sendmail"].ToLower() == "y")
+            {
+                Lowis_Reports_Testing.ObjectLibrary.Helper h2 = new Lowis_Reports_Testing.ObjectLibrary.Helper();
+                h2.LogtoTextFile("Send Email Started");
+                Console.WriteLine("Sending Email .....");
+                try
+                {
+                    h2.LogtoTextFile("adding receipeint");
+                    System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+                    h2.LogtoTextFile("Email list obtained from Top " + ListTo);
+                    string[] recipients = ListTo.Split(';');
+                    h2.LogtoTextFile(recipients[0].ToString());
+                    foreach (string recipient in recipients)
+                    {
+                        if (recipient.Length > 0)
+                        {
+                            message.To.Add(recipient);
+                        }
+                    }
+                    string[] recipientscc = ListCC.Split(';');
+                    h2.LogtoTextFile(recipients[0].ToString());
+                    foreach (string irecipient in recipientscc)
+                    {
+                        if (irecipient.Length > 0)
+                        {
+                            message.CC.Add(irecipient);
+                        }
+                    }
+                    h2.LogtoTextFile("added receipient");
+                    message.From = new System.Net.Mail.MailAddress("ashok.krishna@me.weatherford.com");
+                    System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("mail2.weatherford.com");
+                    smtp.Port = 25;
+                    message.Subject = "License";
+                    message.Body = messagebody;
+                    if (attachmentfilename.Length > 0)
+                    {
+                        var attachment = new System.Net.Mail.Attachment(attachmentfilename);
+                        message.Attachments.Add(attachment);
+                    }
+                    smtp.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    h2.LogtoTextFile("Error in Sending Mails.." + ex.Message);
+                    //  throw new Exception("Error in Sending Mails.." + ex.Message);
+                }
+                h2.LogtoTextFile("Send Email Completed");
+                Console.WriteLine("Sent Email .....");
+            }
+        }
+
+        public static string ReadTextfromTextFile(string filepath)
+        {
+            StringBuilder sb = new StringBuilder();
+            string line = "";
+             StreamReader sr = new StreamReader(filepath);
+            while ( (line = sr.ReadLine()) != null )
+            {
+                sb.Append(line);
+                sb.Append(Environment.NewLine);
+            }
+            sr.Close();
+            return sb.ToString();
+        }
+
+        private static void TerminateProcessByForce(string strprocess)
+        {
+            try
+            {
+                //Assign the name of the process you want to kill on the remote machine
+                string processName = strprocess;
+
+                //Assign the user name and password of the account to ConnectionOptions object
+                //which have administrative privilege on the remote machine.
+                ConnectionOptions connectoptions = new ConnectionOptions();
+                //   connectoptions.Username = @"YourDomainName\UserName";
+                //  connectoptions.Password = "User Password";
+
+                //IP Address of the remote machine
+                string ipAddress = "127.0.0.1";
+                ManagementScope scope = new ManagementScope(@"\\" + ipAddress + @"\root\cimv2", connectoptions);
+
+                //Define the WMI query to be executed on the remote machine
+                SelectQuery query = new SelectQuery("select * from Win32_process where name = '" + processName + "'");
+
+                using (ManagementObjectSearcher searcher = new
+                            ManagementObjectSearcher(scope, query))
+                {
+                    foreach (ManagementObject process in searcher.Get())
+                    {
+
+                        process.InvokeMethod("Terminate", null);
+                        Console.WriteLine("Found and Terminated " + processName);
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Log exception in exception log.
+                //Logger.WriteEntry(ex.StackTrace);
+                Console.WriteLine(ex.StackTrace);
+
+            }
+        }
+
+        private static void TaskKill(string proc)
+        {
+
+            if (proc != "")
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = false;
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "taskkill.exe";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.Arguments = " /f /im "+ "\""+ proc + "\"";
+                try
+                {
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch
+                {
+                    // Log error.
+                }
+            }
+        }
+
+    }
+}
